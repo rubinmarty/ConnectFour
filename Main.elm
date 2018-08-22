461b355e1e33f3216ca57472a812bac788071a99
Marty Rubin <rubinmarty@gmail.com>
2018-08-21 21:03:19 -0400

    Updated some stuff

--- Main.elm
+++ Main.elm
@@ -1,215 +1,206 @@
+module Main exposing (..)
+
 import Grid exposing (..)
+import Html exposing (Html, program)
+import Html.Attributes exposing (class, classList, id)
+import Html.Events exposing (onClick, onMouseOut, onMouseOver)
 import Vector
 
-import Html exposing (Html, program)
-import Html.Attributes exposing (style)
-import Html.Events exposing (onClick)
 
 type alias Owner =
     Maybe Bool
 
+
 type alias Board =
     Grid Owner
+
 
 type alias Model =
     { board : Board
     , player : Bool
-    , victor : Owner
+    , victor : Maybe ( Bool, List Vector.Vector )
+    , highlighted : Maybe Int
     }
 
-type Msg =
-      NoOp
+
+type Msg
+    = NoOp
     | Move Int
+    | Enter Int
+    | Leave
     | Restart
+
 
 main : Program Never Model Msg
 main =
     Html.program
         { init = init ! []
         , update = update
-        , subscriptions = (\_ -> Sub.none)
+        , subscriptions = \_ -> Sub.none
         , view = view
         }
+
 
 init : Model
 init =
     { board = newBoard
     , player = False
     , victor = Nothing
+    , highlighted = Nothing
     }
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
             model ! []
+
         Move x ->
             makeMoveOn model x ! []
+
+        Enter x ->
+            { model | highlighted = Just x } ! []
+
+        Leave ->
+            { model | highlighted = Nothing } ! []
+
         Restart ->
             init ! []
+
 
 view : Model -> Html Msg
 view model =
     let
-        header =
-            Html.h1 [] [Html.text "Connect Four"]
-
-
-        buttonStyle =
-            style
-                [ ("width", "0px") 
-                , ("height", "0") 
-                , ("border-left", "25px solid transparent")
-                , ("border-right", "25px solid transparent")
-                , ("border-top", "25px solid black")
-                , ("display", "inline-block")
-                , ("margin", "0px 1px")
-                ]
-        button x =
-            Html.div [buttonStyle, onClick (Move x)] []
-        buttons =
-            Html.div [] <| List.map button <| List.range 0 6
-
-
-        boxStyle =
-            style
-                [ ("width", "50px")
-                , ("height", "50px")
-                , ("display", "inline-block")
-                , ("margin", "1px")
-                ]
-        color owner =
-            case owner of
-                Nothing ->
-                    "#ABC"
-                Just False ->
-                    "red"
-                Just True ->
-                    "yellow"
-        circleStyle owner =
-            style
-                [ ("width", "50px")
-                , ("height", "50px")
-                , ("border-radius", "25px")
-                , ("background-color", color owner)
-                , ("box-shadow", "1px 1px 1px 1px #00A inset, 1px 1px 1px 1px #33F")
-                ]
-        eltOfOwner owner =
-            Html.div
-                [boxStyle]
-                [Html.div [circleStyle owner] []]
-        combineAcross owner acc =
-            eltOfOwner owner::acc
-        conbineDown rowList acc =
-            Html.div [] rowList::acc
-        boardElts =
-            Grid.foldRightTop combineAcross conbineDown [] [] model.board
-        boardElt =
-            Html.div
-                [ style
-                    [ ("padding", "5px")
-                    , ("background", "blue")
-                    , ("box-shadow", "3px 3px 16px 4px #007")
-                    , ("margin", "10px 0px")
-                    , ("display", "inline-block")
+        eltOfOwner x y owner =
+            Html.td
+                [ class "box", onClick (Move x), onMouseOver (Enter x), onMouseOut Leave ]
+                [ Html.div
+                    [ classList
+                        [ ( "circle", True )
+                        , ( "red", owner == Just False )
+                        , ( "yellow", owner == Just True )
+                        , ( "highlighted", Just x == model.highlighted )
+                        , ( "not-winning", Maybe.map (\( _, lst ) -> List.member ( x, y ) lst) model.victor == Just False )
+                        ]
                     ]
+                    []
                 ]
-                boardElts
+
+        combineAcross ( x, y, owner ) acc =
+            eltOfOwner x y owner :: acc
+
+        conbineDown rowList acc =
+            Html.tr [] rowList :: acc
+
+        boardElts =
+            model.board
+                |> Grid.indexedMap (\( x, y ) owner -> ( x, y, owner ))
+                |> Grid.foldRightTop combineAcross conbineDown [] []
 
         restartButton =
-            Html.div []
-                [ Html.button 
-                    [ onClick Restart
-                    , style [("display", "block"), ("margin", "4px 0px")]
-                    ]
-                    [Html.text "Restart"]
-                ]
+            Html.button
+                [ onClick Restart, id "reset-button" ]
+                [ Html.text "Restart" ]
     in
-        Html.div
-            [ style
-                [ ("text-align", "center")
-                , ("display", "flex")
-                , ("flex-direction", "column")
-                , ("align-items", "center")
-                , ("background", "#ABC")
-                , ("padding", "20px 100px 300px 100px")
-                ]
+    Html.div
+        [ id "content" ]
+        [ Html.div
+            [ id "content-widget" ]
+            [ Html.table [ id "board" ] boardElts
+            , restartButton
             ]
-            [ Html.div [] [header, buttons, boardElt, restartButton]]
+        ]
+
 
 newBoard : Board
 newBoard =
     grid 7 6 Nothing
+
 
 move : Board -> Bool -> Int -> Maybe Board
 move board player x =
     let
         isOwnerless y =
             Grid.lookup x y board
-            |> Maybe.andThen identity
-            |> (==) Nothing
-        minOpenHeight =
-            List.head <| List.filter isOwnerless <| (List.range 0 5)
-    in
-        case minOpenHeight of
-            Nothing ->
-                Nothing
-            Just y ->
-                Just <| Grid.set x y (Just player) board
+                |> Maybe.andThen identity
+                |> (==) Nothing
 
-getWinner : Board -> Owner
+        minOpenHeight =
+            List.head <| List.filter isOwnerless <| List.range 0 5
+    in
+    case minOpenHeight of
+        Nothing ->
+            Nothing
+
+        Just y ->
+            Just <| Grid.set x y (Just player) board
+
+
+getWinner : Board -> Maybe ( Bool, List Vector.Vector )
 getWinner board =
     let
         directions =
-            [ (1,1), (0,1), (1,0), (1,-1) ]
+            [ ( 1, 1 ), ( 0, 1 ), ( 1, 0 ), ( 1, -1 ) ]
 
         spaces : Int -> Int -> Vector.Vector -> List Vector.Vector
         spaces x y v =
-            List.map (\i -> Vector.add (x,y) <| Vector.scale i v) <| List.range 0 3
+            List.map (\i -> Vector.add ( x, y ) <| Vector.scale i v) <| List.range 0 3
 
         victoryInDirection x y v =
             let
                 owner v =
                     Grid.lookupV v board |> Maybe.andThen identity
-                owners = 
+
+                owners =
                     List.map owner <| spaces x y v
             in
-                if List.all ((==) (owner (x, y))) owners
-                then owner (x, y)
-                else Nothing
+            if List.all ((==) (owner ( x, y ))) owners then
+                owner ( x, y )
+                    |> Maybe.andThen (\b -> Just ( b, spaces x y v ))
+
+            else
+                Nothing
 
         combine o1 o2 =
-            case (o1, o2) of
-                (Just b, _) ->
+            case ( o1, o2 ) of
+                ( Just b, _ ) ->
                     Just b
-                (_, Just b) ->
+
+                ( _, Just b ) ->
                     Just b
+
                 _ ->
                     Nothing
 
         victoryFromSpace x y =
             List.map (victoryInDirection x y) directions
-            |> List.foldr combine Nothing
+                |> List.foldr combine Nothing
 
         victory =
             Grid.indexedMap (\v _ -> uncurry victoryFromSpace v) board
-            |> Grid.foldRightTop combine combine Nothing Nothing
+                |> Grid.foldRightTop combine combine Nothing Nothing
     in
-        victory
+    victory
+
 
 makeMoveOn : Model -> Int -> Model
 makeMoveOn model x =
-    if model.victor /= Nothing then model else
-    let
-        newMaybeBoard =
-            move model.board model.player x
-    in
+    if model.victor /= Nothing then
+        model
+
+    else
+        let
+            newMaybeBoard =
+                move model.board model.player x
+        in
         case newMaybeBoard of
             Nothing ->
                 model
+
             Just newBoard ->
-                {model |
-                  board = newBoard
-                , victor = getWinner newBoard
-                , player = not model.player
+                { model
+                    | board = newBoard
+                    , victor = getWinner newBoard
+                    , player = not model.player
                 }
